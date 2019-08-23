dc9092e1aa2f3e08b717cac8e38bcdd1037210ec
mariacarolinafs <mariacarolina_fs@hotmail.com>
2019-08-23 17:30:09 -0300

    Atualização geral (sem readme e sem hacking edition)

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,36 @@
 window.cipher = {
-  // ... 
+  encode: encode,
+  decode: decode
 };
+
+function encode (deslocamento, mensagem) {
+  let arrCodigo = [];
+  let mensagemGrande = mensagem.toUpperCase();
+  let letras = mensagemGrande.split("");
+  letras.forEach(
+    function testeEncode (letras) {
+      if (letras.charCodeAt() != 32) {
+        arrCodigo +=(String.fromCharCode((letras.charCodeAt() - 65 + deslocamento) % 26 + 65));
+      } else {
+        arrCodigo += (String.fromCharCode(letras.charCodeAt()));
+      }
+    }
+  );
+  return arrCodigo;
+}
+
+function decode (deslocamento, mensagem) {
+  let arrCodigoDecode = [];
+  let mensagemGrande = mensagem.toUpperCase();
+  let letras = mensagemGrande.split("");
+  letras.forEach(
+    function testeDecode (letras) {
+      if (letras.charCodeAt() != 32) {
+        arrCodigoDecode +=(String.fromCharCode((letras.charCodeAt() - 65 - deslocamento) % 26 + 65));
+      } else {
+        arrCodigoDecode += (String.fromCharCode(letras.charCodeAt()));
+      }
+    }
+  );
+  return arrCodigoDecode;
+}
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,16 +1,16 @@
-describe('cipher', () => {
+describe("cipher", () => {
 
-  it('should be an object', () => {
-    assert.equal(typeof cipher, 'object');
+  it("should be an object", () => {
+    assert.equal(typeof cipher, "object");
   });
 
-  describe('cipher.encode', () => {
+  describe("cipher.encode", () => {
 
-    it('should be a function', () => {
-      assert.equal(typeof cipher.encode, 'function');
+    it("should be a function", () => {
+      assert.equal(typeof cipher.encode, "function");
     });
 
-    it('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
+    it("should return \"HIJKLMNOPQRSTUVWXYZABCDEFG\" for \"ABCDEFGHIJKLMNOPQRSTUVWXYZ\" with offset 33", () => {
       assert.equal(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG");
     });
 
@@ -46,13 +46,13 @@ describe('cipher', () => {
     // });
   });
 
-  describe('cipher.decode', () => {
+  describe("cipher.decode", () => {
 
-    it('should be a function', () => {
-      assert.equal(typeof cipher.decode, 'function');
+    it("should be a function", () => {
+      assert.equal(typeof cipher.decode, "function");
     });
 
-    it('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset 33', () => {
+    it("should return \"ABCDEFGHIJKLMNOPQRSTUVWXYZ\" for \"HIJKLMNOPQRSTUVWXYZABCDEFG\" with offset 33", () => {
       assert.equal(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     });
 
@@ -67,12 +67,12 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-    // it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
-    //   assert.equal(
-    //     cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg'),
-    //     'abcdefghijklmnopqrstuvwxyz'
-    //   );
-    // });
+    // it("should return \"abcdefghijklmnopqrstuvwxyz\" for \"hijklmnopqrstuvwxyzabcdefg\" with offset 33", () => {
+    //assert.equal(
+    //  cipher.decode(33, "hijklmnopqrstuvwxyzabcdefg"),
+    //  "abcdefghijklmnopqrstuvwxyz"
+    //);
+    //});
 
     // Hacker edition
     //
