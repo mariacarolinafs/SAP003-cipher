27b6ef4987a906be2cb8001454af85e40cee066d
mariacarolinafs <mariacarolina_fs@hotmail.com>
2019-08-28 14:25:14 -0300

    hacker edition letras minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -5,13 +5,16 @@ window.cipher = {
 
 function encode (deslocamento, mensagem) {
   let arrCodigo = [];
-  let mensagemGrande = mensagem.toUpperCase();
-  let letras = mensagemGrande.split("");
+  let letras = mensagem.split("");
   letras.forEach(
     function testeEncode (letras) {
-      if (letras.charCodeAt() != 32) {
+      if (letras.charCodeAt() >= 97 && letras.charCodeAt() <= 122) {
+        arrCodigo +=(String.fromCharCode((letras.charCodeAt() - 97 + deslocamento) % 26 + 97));
+      }
+      else if (letras.charCodeAt() != 32) {
         arrCodigo +=(String.fromCharCode((letras.charCodeAt() - 65 + deslocamento) % 26 + 65));
-      } else {
+      }
+      else {
         arrCodigo += (String.fromCharCode(letras.charCodeAt()));
       }
     }
@@ -21,11 +24,13 @@ function encode (deslocamento, mensagem) {
 
 function decode (deslocamento, mensagem) {
   let arrCodigoDecode = [];
-  let mensagemGrande = mensagem.toUpperCase();
-  let letras = mensagemGrande.split("");
+  let letras = mensagem.split("");
   letras.forEach(
     function testeDecode (letras) {
-      if (letras.charCodeAt() != 32) {
+      if (letras.charCodeAt() >= 97 && letras.charCodeAt() <= 122) {
+        arrCodigoDecode +=(String.fromCharCode((letras.charCodeAt() - 122 - deslocamento) % 26 + 122));
+      }
+      else if (letras.charCodeAt() != 32) {
         arrCodigoDecode +=(String.fromCharCode((letras.charCodeAt() - 90 - deslocamento) % 26 + 90));
       } else {
         arrCodigoDecode += (String.fromCharCode(letras.charCodeAt()));
